52bfcd01c836215a0ec09bb5e1b00c08743f34e1
黄伟平 <huangweiping@kis-MacBook-Pro.local>
2016-03-16 23:09:10 +0800

    correct the insert method should be used

--- db/mysql/VillageHisStatisDataHandler.py
+++ db/mysql/VillageHisStatisDataHandler.py
@@ -4,4 +4,4 @@ def insert_village_his_statistics_data(conn, values):
 	sql = 'insert into village_his_statistics_data(insert_date, village_name, village_avarage_price, on_sale, deal_in_90_days, daikan_in_30_days) values (%s, %s, %s, %s, %s, %s)'
 	
 	dbHandler = DBHandler()
-	dbHandler.insert_single_record(conn, sql, values)
+	dbHandler.insert_batch_record(conn, sql, values)
