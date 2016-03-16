818adda0515500341ad094f6f3e8517dfb9bea06
黄伟平 <huangweiping@kis-MacBook-Pro.local>
2016-03-16 22:09:13 +0800

    correct import error

--- db/mysql/HistoryStatisticsDataHandler.py
+++ db/mysql/HistoryStatisticsDataHandler.py
@@ -1,5 +1,7 @@
-from DBHandler import insert_single_record
+from DBHandler import DBHandler
 
 def insert_history_statistics_data(conn, values):
 	sql = 'insert into history_statistics_data(insert_date, on_sale, deal_in_90_days, deal_in_yesterday, daikan_in_yesterday, new_added_house_yesterday, new_added_customer_yesterday) values (%s, %s, %s, %s, %s, %s, %s)'
-	insert_single_record(conn, sql, values)
+	
+	dbHandler = DBHandler()
+	dbHandler.insert_single_record(conn, sql, values)
--- db/mysql/VillageHisStatisDataHandler.py
+++ db/mysql/VillageHisStatisDataHandler.py
@@ -1,5 +1,7 @@
-from DBHandler import insert_single_record
+from DBHandler import DBHandler
 
 def insert_village_his_statistics_data(conn, values):
 	sql = 'insert into village_his_statistics_data(insert_date, village_name, village_avarage_price, on_sale, deal_in_90_days, daikan_in_30_days) values (%s, %s, %s, %s, %s, %s)'
-	insert_single_record(conn, sql, values)
+	
+	dbHandler = DBHandler()
+	dbHandler.insert_single_record(conn, sql, values)
