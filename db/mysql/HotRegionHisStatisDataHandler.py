9a4dbbacfce83f2e785c49699c96b6e4b4e0e4b0
黄伟平 <huangweiping@kis-MacBook-Pro.local>
2016-06-25 23:47:29 +0800

    upate data handler for hot region historial crawler

--- db/mysql/HotRegionHisStatisDataHandler.py
+++ db/mysql/HotRegionHisStatisDataHandler.py
@@ -1,6 +1,6 @@
 from DBHandler import DBHandler
 
-def insert_host_region_his_statistics_data(conn, values):
+def insert_hot_region_his_statistics_data(conn, values):
 	sql = 'insert into hot_region_his_statistics_data(insert_date, region_name, region_avarage_price, on_sale, deal_in_90_days, added_in_7_days, daikan_in_7_days, deal_in_last_month, deal_compare_last_month, deal_compare_last_year) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
 	
 	dbHandler = DBHandler()
