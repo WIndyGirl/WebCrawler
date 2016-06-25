55dbe97bfc278bdca3a2c681198cf9e3dd6c989b
黄伟平 <huangweiping@kis-MacBook-Pro.local>
2016-06-25 21:12:07 +0800

    update DB handler for District Historical data crawler

--- db/mysql/DistrictHisStatisDataHandler.py
+++ db/mysql/DistrictHisStatisDataHandler.py
@@ -1,7 +1,7 @@
 from DBHandler import DBHandler
 
 def insert_district_his_statistics_data(conn, values):
-	sql = 'insert into district_his_statistics_data(insert_date, district, district_avarage_price, deal_in_last_month, on_sale, deal_in_90_days, deal_in_yesterday, daikan_in_yesterday, daika_rate) values (%s, %s, %s, %s, %s, %s, %s, %s, %s)'
+	sql = 'insert into district_his_statistics_data(insert_date, district, district_avarage_price, deal_in_last_month, deal_compare_last_month, deal_compare_last_year, on_sale, deal_in_90_days, deal_in_yesterday, daikan_in_yesterday) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
 	
 	dbHandler = DBHandler()
 	dbHandler.insert_batch_record(conn, sql, values)
