fa7025c1a8b5dd1914c145acfed8411d601be962
黄伟平 <huangweiping@kis-MacBook-Pro.local>
2016-07-21 23:47:22 +0800

    update DataHandler for JianWei Deal Data Crawler

--- db/mysql/JianWeiDealDataHandler.py
+++ db/mysql/JianWeiDealDataHandler.py
@@ -4,4 +4,4 @@ def insert_jianwei_deal_data(conn, values):
 	sql = 'insert into jianwei_deal_data(insert_date, verify_num, verify_area, house_verify_num, house_verify_area, sign_num, sign_area, house_sign_num, house_sign_area) values (%s, %s, %s, %s, %s, %s, %s, %s, %s)'
 	
 	dbHandler = DBHandler()
-	dbHandler.insert_batch_record(conn, sql, values)
+	dbHandler.insert_single_record(conn, sql, values)
